ade829c5740593830f083197ec624af50da3d606
luojimeng <luojimeng@advanpro.hk>
2020-03-16 10:49:20 +0800

    错误码定义

--- src/main/java/com/bms/ErrorCodes.java
+++ src/main/java/com/bms/ErrorCodes.java
@@ -17,13 +17,27 @@ import com.bms.common.exception.ServiceException;
  * @date 2020/3/09
  */
 public enum ErrorCodes {
+    /**
+     * common(1000-9999).
+     */
     DATA_NOT_EXIST(1000, "记录不存在"),
     DATABASE_ERR(1001, "操作数据错误"),
     SESSION_INVALID(1002, "Session会话无效或过期，请重新登录"),
     OSS_FILE_EMPTY(1003, "文件为空或无效"),
     USER_STATUS_DISABLED(1004, "用户禁用"),
+
+
+    /**
+     * sys(10000-19999).
+     */
     PASSWD_ERR(10000, "密码错误"),
     ACCOUNT_NOT_EXIST(10001, "账号不存在");
+
+
+    /**
+     * industry(20000-29999).
+     */
+
 
     private int code;
     private String message;
