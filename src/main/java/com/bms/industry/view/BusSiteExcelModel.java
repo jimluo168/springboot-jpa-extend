cd0f2f180a1461ab24d8e0091d0e7ecceb638c1e
zouyongcan <zouyongcan@advanpro.hk>
2020-03-24 09:28:01 +0800

    修改

--- src/main/java/com/bms/industry/view/BusSiteExcelModel.java
+++ src/main/java/com/bms/industry/view/BusSiteExcelModel.java
@@ -4,9 +4,15 @@ import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.bms.common.domain.BaseEntity;
+import com.bms.entity.BusSite;
 import com.bms.entity.Organization;
+import com.bms.industry.controller.BusSiteController;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
 
 /**
  * 公交站点导出excel的model
@@ -16,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Data
 public class BusSiteExcelModel extends BaseEntity {
+    private static final Logger logger = LoggerFactory.getLogger(BusSiteExcelModel.class);
 
     @ColumnWidth(50)
     @ExcelProperty(value = "站点名称", index = 0)
@@ -45,6 +52,17 @@ public class BusSiteExcelModel extends BaseEntity {
     @ExcelProperty(value = "所在区域", index = 2)
     private String region;
 
+    /**
+     * 经度.
+     */
+    @ExcelIgnore
+    private Float longitude;
+    /**
+     * 纬度.
+     */
+    @ExcelIgnore
+    private Float latitude;
+
     @ColumnWidth(50)
     @ExcelProperty(value = "经纬度", index = 3)
     private String longitudeAndLatitude;
@@ -58,6 +76,7 @@ public class BusSiteExcelModel extends BaseEntity {
      */
     @ExcelIgnore
     private Integer status;
+
     @ColumnWidth(15)
     @ExcelProperty(value = "状态", index = 5)
     private String statusText;
@@ -96,17 +115,21 @@ public class BusSiteExcelModel extends BaseEntity {
         return "";
     }
 
+    public String getLongitudeAndLatitude() {
+        return longitude.toString()+ "," + latitude.toString();
+    }
+
     public String getStatusText() {
         if (status == null) {
             return "";
         }
-        if (status == Organization.STATUS_TO_BE_AUDIT) {
+        if (status == BusSite.STATUS_TO_BE_AUDIT) {
             return "待审核";
         }
-        if (status == Organization.STATUS_PASS_AUDIT) {
+        if (status == BusSite.STATUS_PASS_AUDIT) {
             return "已通过";
         }
-        if (status == Organization.STATUS_UN_AUDIT) {
+        if (status == BusSite.STATUS_UN_AUDIT) {
             return "未通过";
         }
         return status.toString();
